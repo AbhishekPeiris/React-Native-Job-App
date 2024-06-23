97504922231746bfc03cb877f54ea7fd76504609
Abhishek Peiris <abhisheklpeiris@gmail.com>
2024-06-23 15:34:09 +0530

    Update index.js

--- Job_App/app/index.js
+++ Job_App/app/index.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { View, Text, ScrollView, SafeAreaView } from 'react-native';
 import { Stack, useRouter} from 'expo-router';
 
-import { COLORS, icons, images, SIZE } from '../constants';
+import { COLORS, icons, images, SIZE, SIZES } from '../constants';
 import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
 
 const Home = () => {
@@ -21,6 +21,14 @@ const Home = () => {
                     headerTitle: "",
                 }}
             />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{ flex:1, padding:SIZES.medium }}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     );
 }
